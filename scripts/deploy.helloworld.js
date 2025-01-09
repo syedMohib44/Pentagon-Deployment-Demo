8237645882e276256e5a8cd3fb356c7d55acc740
syed mohib <syedMohib44>
2025-01-09 17:52:02 +0500

    Deployment tested

--- scripts/deploy.helloworld.js
+++ scripts/deploy.helloworld.js
@@ -4,18 +4,18 @@ const hre = require("hardhat");
 async function main() {
 
   const [addr1] = await hre.ethers.getSigners();
-  // const houseContract = await ethers.getContractFactory("HelloWorld");
-  // this.houseContract = await houseContract.connect(addr1).deploy();
+  const helloworldContract = await ethers.getContractFactory("HelloWorld");
+  this.helloworldContract = await helloworldContract.connect(addr1).deploy();
 
-  // console.log("House deployed to:", this.houseContract.target);
+  console.log("Hello World deployed to:", this.helloworldContract.target);
   await new Promise(r => setTimeout(r, 60000));
 
   try {
     await hre.run("verify:verify", {
-      address: '0x89899D6A337ac8d1bddaB62B86D258A6e769aBF8'
+      address: this.helloworldContract.target
     });
   } catch (err) {
-console.log(err)
+    console.log(err)
   }
 }
 
